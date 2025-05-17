a50a5858a1063ab82978268db6153b8a89eba703
Fabiola Zuñiga <fzunigav@ucenfotec.ac.cr>
2025-05-17 11:34:02 -0600

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Entities;
+using System.Data.Common;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Instancia del gato
+
+        var myCat = new Cat() { Id = 1, Name = "Huevo" };
+
+        Console.WriteLine("Id del Gato: " + myCat.Id);
+        Console.WriteLine("Nombre del gato: " + myCat.Name);
+
+        Console.WriteLine("Ingrese una descripción para el gato: ");
+        myCat.Description = Console.ReadLine();
+
+    }
+}
---
+++ Entities/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Cat : Animal
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Birth { get; set; }
+
+        public Cat() { }
+
+
+    }
+}
